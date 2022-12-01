c8ea456fb58c9cbd2dcb0f9c81a5c57ff36cff2a
Ruslan Nadyrshyn <ruslannadyrshin1999@gmail.com>
2022-12-01 14:31:57 +0200

    refactoring

--- scripts.php
+++ scripts.php
@@ -5,67 +5,8 @@ include "env/.env.php";
     const APPID = "<?php echo $APPID; ?>";
     const WEATHER_SOURCE = "http://openweathermap.org/img/wn/";
     const PNG_ENDING = "@2x.png";
-
-
-    // sort(param) {
-        
-    // }
-
-    function createCounterList(counterArray) {
-        console.log("createCounterList");
-        var $nav = $("<nav class=\"navigator-block\"></nav>");
-        
-        for (let i = 0; i < counterArray.length; i++) {
-            const element = counterArray[i];
-
-            var $counterItem = $("<a class=\"navigator-item\"></a>");
-            
-            if (i == 0) $counterItem.addClass("selected");
-
-            $counterItem.append(element);
-            $counterItem.attr('id', "counter" + element);
-            $counterItem.click(function () {
-                clickCounter(element);
-            });
-            $nav.append($counterItem);
-        }
-        return $nav;
-    }
-
-    function clickCounter(element) {
-        var elements = document.getElementsByClassName('selected');
-        for (let j = 0; j < elements.length; j++)
-            elements[j].classList.toggle("selected");
-
-        document.getElementById("counter" + element).classList.toggle("selected");
-        document.getElementById("counter").value = element;
-
-        updateTable();
-    }
-
-    function printNavCounter () {
-        var counterArray = [20, 50, 100, 200, 500, 1000];
-        var $counterList = createCounterList(counterArray);
-        
-        $("#navCounter").empty();
-        $counterList.appendTo($("#navCounter"));
-    }
-
-    var loadData = function () {
-        $.ajax({													// ajax-запит до бази даних для динамічного 
-            type: "GET",                                            // виводу даних в таблицю "Дані датчика BME280".
-            url: "database/extract.php",									// Виклик файла extract.php, в якому виконується запит до БД
-            dataType: "json",
-            success: function (result) {
-
-                $("#temp").text(result.temp_bme280 + ' °С');
-                $("#press").text(result.press_bme280 + ' гПа');
-                $("#alt").text(result.alt_bme280 + ' м');
-                $("#hum").text(result.hum_bme280 + ' %');
-                setTimeout(loadData, 2000);                         // Рекурсійний виклик функції для оновлення інформації кожні 2 секунди
-            }
-        });
-    };
+    
+    /* ------------------------------- Weather -------------------------------*/
 
     var loadWeather = function (city) {
         $.ajax({													    // ajax-запит до бази даних для динамічного 
@@ -98,6 +39,139 @@ include "env/.env.php";
         });
     };
 
+    function changeLocation(newLocation) {
+        document.getElementById("location").innerHTML = newLocation;
+        loadWeather(newLocation);						            // Виклик функції для створення таблиці з даними погоди
+    }
+
+    /* ------------------------------- Current_BME280 -------------------------------*/
+
+    var loadData = function () {
+        $.ajax({													// ajax-запит до бази даних для динамічного 
+            type: "GET",                                            // виводу даних в таблицю "Дані датчика BME280".
+            url: "database/extract.php",									// Виклик файла extract.php, в якому виконується запит до БД
+            dataType: "json",
+            success: function (result) {
+                $("#temp").text(result.temp_bme280 + ' °С');
+                $("#press").text(result.press_bme280 + ' гПа');
+                $("#alt").text(result.alt_bme280 + ' м');
+                $("#hum").text(result.hum_bme280 + ' %');
+                setTimeout(loadData, 2000);                         // Рекурсійний виклик функції для оновлення інформації кожні 2 секунди
+            }
+        });
+    };
+
+    /* ------------------------------- Navigator -------------------------------*/
+
+    function printNavCounter () {
+        var counterArray = [20, 50, 100, 200, 500, 1000];
+        var $counterList = createCounterList(counterArray);
+        
+        $("#navCounter").empty();
+        $counterList.appendTo($("#navCounter"));
+    }
+
+    function printNavPages(count) {
+        if (count < 0) return;
+        var numOfPages = getNumOfPages(count);
+        document.getElementById("numOfPages").innerText = numOfPages;
+        document.getElementById("page").innerText = 1;
+
+        var $pages = createNavList(numOfPages); 
+
+        $("#navPages").empty();
+        $pages.appendTo($("#navPages"));
+    }
+
+    function createCounterList(counterArray) {
+        console.log("createCounterList");
+        var $nav = $("<nav class=\"navigator-block\"></nav>");
+        
+        for (let i = 0; i < counterArray.length; i++) {
+            const element = counterArray[i];
+
+            var $counterItem = $("<a class=\"navigator-item\"></a>");
+            
+            if (i == 0) $counterItem.addClass("selected");
+
+            $counterItem.append(element);
+            $counterItem.attr('id', "counter" + element);
+            $counterItem.click(function () {
+                clickCounter(element);
+            });
+            $nav.append($counterItem);
+        }
+        return $nav;
+    }
+
+    function createNavList(numOfPages) {                // Функція для створення таблиці, яка приймає               
+        var $nav = $("<nav class=\'navigator-block pages\'></nav>");          // масив даних таблиці(data) та головний рядок(header)
+
+        for (let i = 1; i <= numOfPages; i++) {
+            var $navItem = $("<a class=\"navigator-item\"></a>");
+
+            if (i == 1) $navItem.addClass("selected-page");
+
+            $navItem.append(i);
+            $navItem.attr('id', "page" + i);
+            $navItem.click(function () {
+                clickPage(i);
+            });
+            $nav.append($navItem);
+        }
+        return $nav;
+    }
+
+    function clickCounter(element) {
+        var elements = document.getElementsByClassName('selected');
+        for (let j = 0; j < elements.length; j++)
+            elements[j].classList.toggle("selected");
+
+        document.getElementById("counter" + element).classList.toggle("selected");
+        document.getElementById("counter").value = element;
+
+        updateTable();
+    }
+
+    function clickPage(i) {
+        var elements = document.getElementsByClassName('selected-page');
+        for (let j = 0; j < elements.length; j++)
+            elements[j].classList.toggle("selected-page");
+
+        var element = document.getElementById("page" + i).classList.toggle("selected-page");
+
+        document.getElementById("page").innerText = i;
+
+        updateTable();
+    }
+
+    var getNumOfPages = function (count) {
+        var numOfPages = 0;
+        $.ajax({
+            async: false,											// ajax-запит до бази даних для динамічного 
+            type: "GET",                                            // виводу даних в таблицю "Дані датчика BME280".
+            url: "database/get_num_of_rows.php?count=" + count,		// Виклик файла extract.php, в якому виконується запит до БД
+            dataType: "json",
+            success: function (result) {
+                var rows = Number(result.rows);
+                numOfPages = Math.ceil(rows / count);
+            }
+        });
+        return numOfPages;
+    };
+
+    /* -------------------------------- Database --------------------------------*/
+
+    function updateTable() {
+        var page = document.getElementById("page").innerText;
+        var counter = document.getElementById("counter").value;
+        console.log("counter:", counter);
+        // var param = document.getElementById("param").value;
+        var order = document.getElementById("order").value;
+        console.log("order:", order);
+        loadTable(page, counter, "date", order);
+    }
+    
     function loadTable(page, count, param, order) {
         var _page = "1";
         var _count = "20";
@@ -125,63 +199,6 @@ include "env/.env.php";
         var $table = createTable(data, dbHeader, false);            // виклик ф-ції createTable() з відповідними даними
         $("#dbTable").empty();
         $table.appendTo($("#dbTable"));
-    }
-
-    function printNavPages(count) {
-        if (count < 0) return;
-        var numOfPages = getNumOfPages(count);
-        document.getElementById("numOfPages").innerText = numOfPages;
-        document.getElementById("page").innerText = 1;
-
-        var $pages = createNavList(numOfPages); 
-
-        $("#navPages").empty();
-        $pages.appendTo($("#navPages"));
-    }
-
-    var getNumOfPages = function (count) {
-        var numOfPages = 0;
-        $.ajax({
-            async: false,											// ajax-запит до бази даних для динамічного 
-            type: "GET",                                            // виводу даних в таблицю "Дані датчика BME280".
-            url: "database/get_num_of_rows.php?count=" + count,		// Виклик файла extract.php, в якому виконується запит до БД
-            dataType: "json",
-            success: function (result) {
-                var rows = Number(result.rows);
-                numOfPages = Math.ceil(rows / count);
-            }
-        });
-        return numOfPages;
-    };
-
-    function printCharts(data) {                                    // Функція для створення графіків
-        var res = fetchResult(data);
-        drawCharts(res, res.date);
-    }
-
-    function changeLocation(newLocation) {
-        document.getElementById("location").innerHTML = newLocation;
-        loadWeather(newLocation);						            // Виклик функції для створення таблиці з даними погоди
-    }
-
-    function printRow(object, isHeader) {                           // Допоміжна функція для створення рядка таблиці
-        var $line = $("<tr></tr>");
-        if (isHeader) object.forEach(element =>
-            $line.append($("<th class='sticky'></th>").html(element)));
-        else {
-            for (const key in object)
-                if (Object.hasOwnProperty.call(object, key)) {
-                    if (key == "date_bme280") {
-                        datetime = object[key].split(" ");
-                        date = datetime[0];
-                        time = datetime[1];
-                        $line.append($("<td></td>").html(date));
-                        $line.append($("<td></td>").html(time));
-                    } else $line.append($("<td></td>").html(object[key]));
-                }
-
-        }
-        return $line;
     }
 
     function createTable(data, header, isVertical) {                // Функція для створення таблиці, яка приймає               
@@ -213,45 +230,32 @@ include "env/.env.php";
         return $table;
     }
 
-    function updateTable() {
-        var page = document.getElementById("page").innerText;
-        var counter = document.getElementById("counter").value;
-        console.log("counter:", counter);
-        // var param = document.getElementById("param").value;
-        var order = document.getElementById("order").value;
-        console.log("order:", order);
-        loadTable(page, counter, "date", order);
-    }
+    function printRow(object, isHeader) {                           // Допоміжна функція для створення рядка таблиці
+        var $line = $("<tr></tr>");
+        if (isHeader) object.forEach(element =>
+            $line.append($("<th class='sticky'></th>").html(element)));
+        else {
+            for (const key in object)
+                if (Object.hasOwnProperty.call(object, key)) {
+                    if (key == "date_bme280") {
+                        datetime = object[key].split(" ");
+                        date = datetime[0];
+                        time = datetime[1];
+                        $line.append($("<td></td>").html(date));
+                        $line.append($("<td></td>").html(time));
+                    } else $line.append($("<td></td>").html(object[key]));
+                }
 
-
-    function createNavList(numOfPages) {                // Функція для створення таблиці, яка приймає               
-        var $nav = $("<nav class=\'navigator-block pages\'></nav>");          // масив даних таблиці(data) та головний рядок(header)
-
-        for (let i = 1; i <= numOfPages; i++) {
-            var $navItem = $("<a class=\"navigator-item\"></a>");
-
-            if (i == 1) $navItem.addClass("selected-page");
-
-            $navItem.append(i);
-            $navItem.attr('id', "page" + i);
-            $navItem.click(function () {
-                clickPage(i);
-            });
-            $nav.append($navItem);
         }
-        return $nav;
+        return $line;
     }
 
-    function clickPage(i) {
-        var elements = document.getElementsByClassName('selected-page');
-        for (let j = 0; j < elements.length; j++)
-            elements[j].classList.toggle("selected-page");
 
-        var element = document.getElementById("page" + i).classList.toggle("selected-page");
+    /* --------------------------------- Charts ---------------------------------*/
 
-        document.getElementById("page").innerText = i;
-
-        updateTable();
+    function printCharts(data) {                                    // Функція для створення графіків
+        var res = fetchResult(data);
+        drawCharts(res, res.date);
     }
 
     function fetchResult(result) {                                  // Допоміжна функція для відокремлення окремих 
@@ -270,6 +274,36 @@ include "env/.env.php";
         });
 
         return { temp, press, alt, hum, date };
+    }
+
+    function drawCharts(res, labels) {                              // Ф-ція створення графіків
+        for (const key in res)
+            if (Object.hasOwnProperty.call(res, key))
+                res[key].reverse();
+        [{
+            id: 'chart-temp',	                                // Графік температури
+            color: 'yellow',
+            data: res.temp,
+        }, {
+            id: 'chart-press',	                                // Графік тиску
+            color: 'red',
+            data: res.press,
+        }, {
+            id: 'chart-alt', 	                                // Графік висоти
+            color: 'green',
+            data: res.alt,
+        }, {
+            id: 'chart-hum', 	                                // Графік вологості
+            color: 'blue',
+            data: res.hum,
+        }].forEach(function (details) {
+            let chartStatus = Chart.getChart(details.id);
+            if (chartStatus != undefined) chartStatus.destroy();
+
+            var ctx = document.getElementById(details.id).getContext('2d');
+            var config = createConfig(labels, details.data, details.color);
+            new Chart(ctx, config);
+        });
     }
 
     function createConfig(labels, data, colorName) {                // допоміжна ф-ція для налаштування виводу графіків
@@ -299,36 +333,7 @@ include "env/.env.php";
         };
     }
 
-    function drawCharts(res, labels) {                              // Ф-ція створення графіків
-        for (const key in res)
-            if (Object.hasOwnProperty.call(res, key))
-                res[key].reverse();
 
-        [{
-            id: 'chart-temp',	                                // Графік температури
-            color: 'yellow',
-            data: res.temp,
-        }, {
-            id: 'chart-press',	                                // Графік тиску
-            color: 'red',
-            data: res.press,
-        }, {
-            id: 'chart-alt', 	                                // Графік висоти
-            color: 'green',
-            data: res.alt,
-        }, {
-            id: 'chart-hum', 	                                // Графік вологості
-            color: 'blue',
-            data: res.hum,
-        }].forEach(function (details) {
-            let chartStatus = Chart.getChart(details.id);
-            if (chartStatus != undefined) chartStatus.destroy();
-
-            var ctx = document.getElementById(details.id).getContext('2d');
-            var config = createConfig(labels, details.data, details.color);
-            new Chart(ctx, config);
-        });
-    }
 
     function toggleChart(id) {                                      // Функція збільшення графіка при натисканні
         var element = document.getElementById(id.id);
