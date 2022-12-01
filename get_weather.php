a57bfa11343f1e680a401ca9db4f7021c90437c1
RuslanNadyrshyn <ruslan.nadyrshyn@infineon.com>
2022-12-02 01:11:21 +0200

    move requests to db to another file

---
+++ get_weather.php
@@ -0,0 +1,11 @@
+<?php
+include "env/.env.php";         // Підключення файлу зі змінною $APPID
+
+if (isset($_GET['city']))		// Зчитування змінної city з URL
+	$city = $_GET['city'];		
+
+// Запит до API для отримання даних погоди обраного міста
+$result = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$APPID&units=metric&lang=ua");
+
+echo json_encode($result);      // Вивід даних у форматі json
+?>
