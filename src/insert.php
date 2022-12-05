2e45e562edb42b8db5f5690e2c29239563c96a7e
Ruslan Nadyrshyn <ruslannadyrshin1999@gmail.com>
2022-12-05 16:05:56 +0200

    change insert.php

--- src/insert.php
+++ src/insert.php
@@ -11,23 +11,23 @@ if (isset($_GET['hum']))
 	$hum = $_GET['hum'];                           		// Створення змінної Вологість з URL сторінки
 if (isset($_GET['counter']))
 	$counter = $_GET['counter'];                  		// Створення змінної Лічильник з URL сторінки								
-if (isset($_GET['maxcount']))
-	$maxcount = $_GET['maxcount'];						// Створення змінної Максимального лічильника з URL сторінки для запису у БД bme280
 
 $query = $conn->query("SELECT * FROM bme280_current");	// Створення запиту до БД
 
-if(mysqli_num_rows($query) != 0){
-	$sql = "UPDATE bme280_current SET temp_bme280 = $temp, press_bme280 = $press, alt_bme280 = $alt, hum_bme280 = $hum";
-	$result = mysqli_query($conn, $sql);				// Оновлення даних у таблиці bme280_current
+if(mysqli_num_rows($query) != 0) {
+	$sql = "UPDATE bme280_current SET temp_bme280 = $temp, press_bme280 = $press, alt_bme280 = $alt, hum_bme280 = $hum, max_count = 10";
+	mysqli_query($conn, $sql);				// Оновлення даних у таблиці bme280_current
 }
-else{
+else {
+	$result = mysqli_query($conn, "SELECT max_count FROM bme280_current");
+	$maxcount = $result->max_count;
 	$sql = "INSERT INTO bme280_current (temp_bme280, press_bme280, alt_bme280, hum_bme280) VALUES ($temp, $press, $alt, $hum)";
-	$result = mysqli_query($conn, $sql);				// Внесення даних до таблиці
+	mysqli_query($conn, $sql);				// Внесення даних до таблиці
 }
 	
-if($counter==$maxcount){								// Внесення даних до таблиці bme280
+if($counter % $maxcount){								// Внесення даних до таблиці bme280
 	$sql = "INSERT INTO bme280 (temp_bme280, press_bme280, alt_bme280, hum_bme280) VALUES ($temp, $press, $alt, $hum)";
-	$result = mysqli_query($conn, $sql);
+	mysqli_query($conn, $sql);
 }
 
 echo $maxcount;
