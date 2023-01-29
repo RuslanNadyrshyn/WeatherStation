5818ffbb347b6023c3c51a59e12f890a1acbe292
RuslanNadyrshyn <ruslan.nadyrshyn@infineon.com>
2023-01-29 13:38:48 +0200

    fix sh

--- src/check_connection.sh
+++ src/check_connection.sh
@@ -2,10 +2,10 @@
 
 variable=TRUE;
 while variable==TRUE; do
-  echo "running check_connection.php"
+  date +"%Y/%m/%d %H:%M:%S Running check_connection.php"
   temp=$(/usr/bin/php -f ./check_connection.php)
-  if [[ "${temp:-}" = "0" ]]; then 
-    echo 'No connection, exiting'
+  if [[ "${temp:-}" = "0" ]]; then
+    date +"%Y/%m/%d %H:%M:%S No connection, exiting"
     exit 1
   fi
   sleep 5m
