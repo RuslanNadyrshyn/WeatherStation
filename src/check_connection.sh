01f345a78c309a3668659709a9fc5623902a5b05
RuslanNadyrshyn <ruslan.nadyrshyn@infineon.com>
2023-01-29 12:52:10 +0200

    add exit message

--- src/check_connection.sh
+++ src/check_connection.sh
@@ -4,6 +4,6 @@ variable=TRUE;
 while variable==TRUE; do
   echo "running check_connection.php"
   temp=$(/usr/bin/php -f ./check_connection.php)
-  [[ temp=='0' ]] && { exit 1; }
+  [[ temp=='0' ]] && { echo 'No connection, exiting'; exit 1; }
   sleep 5m
 done
